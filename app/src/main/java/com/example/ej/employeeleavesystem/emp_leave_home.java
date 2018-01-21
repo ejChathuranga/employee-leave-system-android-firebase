e04492b21e10a6fdefb58aeac9ca9dacd2a0cbd3
ejChathuranga <ejcindunil@gmail.com>
2018-01-21 11:01:21 +0530

    login with sharedpreferences

--- app/src/main/java/com/example/ej/employeeleavesystem/emp_leave_home.java
+++ app/src/main/java/com/example/ej/employeeleavesystem/emp_leave_home.java
@@ -1,5 +1,6 @@
 package com.example.ej.employeeleavesystem;
 
+import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,6 +17,13 @@ import com.example.ej.employeeleavesystem.emp_leave_tabs.emp_leave_home_old;
 public class emp_leave_home extends AppCompatActivity {
     private static final String  TAG = "emp_leave_home";
 
+
+    private final static String USER_MANEGER = "Manager";
+    private final static String USER_LEAD = "Lead";
+    private final static String USER_EMPLOYEE = "Employee";
+    private static String USER_ROLE = "";
+    public SharedPreferences myPreferences;
+
     private SectionPageAdapter mSectionPageAdapter;
     private ViewPager mViewPager;
     @Override
@@ -23,6 +31,8 @@ public class emp_leave_home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emp_leave_home);
         Log.d("TAG","onCreate: Loading..");
+        myPreferences = getSharedPreferences(emp_leave_login.myPREF,0);
+        USER_ROLE = myPreferences.getString(emp_leave_login.PREF_ROLE,"");
 
         mSectionPageAdapter = new SectionPageAdapter(getSupportFragmentManager());
 
@@ -36,6 +46,19 @@ public class emp_leave_home extends AppCompatActivity {
     private void setupViewPager(ViewPager mViewPager) {
 //        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
 
+        switch (USER_ROLE){
+            case USER_MANEGER:{
+
+                break;
+            }
+            case USER_LEAD:{
+                break;
+            }
+            case USER_EMPLOYEE:{
+                break;
+            }
+        }
+
         mSectionPageAdapter.addFragment(new emp_leave_home_approve(), "Approved");
         mSectionPageAdapter.addFragment(new emp_leave_home_new(), "New");
         mSectionPageAdapter.addFragment(new emp_leave_home_old(), "Old");
@@ -43,128 +66,3 @@ public class emp_leave_home extends AppCompatActivity {
         mViewPager.setAdapter(mSectionPageAdapter);
     }
 }
-//
-//public class emp_leave_home extends AppCompatActivity {
-//
-//    /**
-//     * The {@link android.support.v4.view.PagerAdapter} that will provide
-//     * fragments for each of the sections. We use a
-//     * {@link FragmentPagerAdapter} derivative, which will keep every
-//     * loaded fragment in memory. If this becomes too memory intensive, it
-//     * may be best to switch to a
-//     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-//     */public static Context mContext;
-//    private SectionsPagerAdapter mSectionsPagerAdapter;
-//
-//    /**
-//     * The {@link ViewPager} that will host the section contents.
-//     */
-//    private ViewPager mViewPager;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_emp_leave_home);
-//
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        // Create the adapter that will return a fragment for each of the three
-//        // primary sections of the activity.
-//        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-//
-//        // Set up the ViewPager with the sections adapter.
-//        mViewPager = (ViewPager) findViewById(R.id.container);
-//        mContext = getBaseContext();
-//        mViewPager.setAdapter(mSectionsPagerAdapter);
-//
-//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-//
-//        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-//        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//
-//    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_emp_leave_home, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//
-//    /**
-//     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-//     * one of the sections/tabs/pages.
-//     */
-//    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-//
-//        public SectionsPagerAdapter(FragmentManager fm) {
-//            super(fm);
-//        }
-//
-//        @Override
-//        public Fragment getItem(int position) {
-//            switch(position){
-//                case 0:
-//                    emp_leave_home_new tab1 = new emp_leave_home_new();
-//                    return tab1;
-//                case 1:
-//                    emp_leave_home_approve tab2 = new emp_leave_home_approve();
-//                    return tab2;
-//                case 2:
-//                    emp_leave_home_old tab3 = new emp_leave_home_old();
-//                    return tab3;
-//                default:
-//                    return null;
-//            }
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            // Show 3 total pages.
-//            return 3;
-//        }
-//
-//        public CharSequence getPageTitle(int position){
-//            switch (position){
-//                case 0:
-////                    return "New";
-//                    return (mContext.getString(R.string.tab_text_1));
-//                case 1:
-//                    return "Approve";
-//                case 2:
-//                    return "Old";
-//
-//            }
-//            return null;
-//        }
-//    }
-//}
--- app/src/main/java/com/example/ej/employeeleavesystem/emp_leave_login.java
+++ app/src/main/java/com/example/ej/employeeleavesystem/emp_leave_login.java
@@ -2,6 +2,8 @@ package com.example.ej.employeeleavesystem;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +14,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 
 public class emp_leave_login extends AppCompatActivity implements View.OnClickListener {
@@ -19,6 +36,17 @@ public class emp_leave_login extends AppCompatActivity implements View.OnClickLi
     private EditText empUsername, empPass;
     private TextView empForgotPass, empNewAcc;
     private Button empLogin;
+
+    private String sendURL = "http://10.0.2.2/isuru_leave/leave_login_user.php";
+
+
+    public static final String myPREF = "myPreferences";
+    public static final String PREF_NAME = "myName";
+    public static final String PREF_USERNAME = "myUserName";
+    public static final String PREF_ROLE = "myRole";
+    public static SharedPreferences myPreferences;
+    public static SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +60,9 @@ public class emp_leave_login extends AppCompatActivity implements View.OnClickLi
         empNewAcc = (TextView)findViewById(R.id.tv_firstNewAccount);
         empLogin = (Button) findViewById(R.id.btn_login);
 
+        myPreferences = getSharedPreferences(myPREF, 0);
+        editor = myPreferences.edit();
+
         empLogin.setOnClickListener(this);
         empNewAcc.setOnClickListener(this);
         empForgotPass.setOnClickListener(this);
@@ -42,7 +73,8 @@ public class emp_leave_login extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()){
             case R.id.btn_login:{
                 if(!isEmpty(empUsername)|| !isEmpty(empPass)){
-                    startActivity(new Intent(emp_leave_login.this, emp_leave_home.class));
+                    new addSoldierAsync().execute();
+//                    startActivity(new Intent(emp_leave_login.this, emp_leave_home.class));
 
                 }else{
                     empUsername.setError("Please fill-out");
@@ -59,6 +91,109 @@ public class emp_leave_login extends AppCompatActivity implements View.OnClickLi
                 startActivity( new Intent(emp_leave_login.this, emp_leave_emp_regiter.class));
                 break;
             }
+        }
+    }
+
+    class addSoldierAsync extends AsyncTask<String, String, String> {
+
+        String username = empUsername.getText().toString();
+        String pass = empPass.getText().toString();
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Object obj = JSONValue.parse(s);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            Boolean success = (Boolean) jsonObject.get("success");
+            String name = (String) jsonObject.get("name");
+            String role = (String) jsonObject.get("role");
+
+            editor.putString(PREF_USERNAME,username);
+            editor.putString(PREF_NAME,name);
+            editor.putString(PREF_ROLE,role);
+            editor.commit();
+
+            if(success){
+                startActivity(new Intent(emp_leave_login.this, emp_leave_home.class));
+                Toast.makeText(emp_leave_login.this, "Successfully logged", Toast.LENGTH_SHORT).show();
+
+            }else{
+                Toast.makeText(emp_leave_login.this, "Please check network connection", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            // set data to send into the server
+            String dataJson = null;
+            try {
+                dataJson = URLEncoder.encode("username", "UTF-8") + "="+ URLEncoder.encode(username, "UTF-8");
+                dataJson += "&" + URLEncoder.encode("pass", "UTF-8") + "="+ URLEncoder.encode(pass, "UTF-8");
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            // define URL where we send those data
+            URL url = null;
+            try {
+                url = new URL(sendURL);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            String response = "";
+            BufferedReader bufferedReader=null;
+
+            try {
+                // Send POST data request
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setRequestMethod("POST");
+//                OutputStreamWriter wr = new OutputStreamWriter(httpURLConnection.getOutputStream());
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                Log.e("Send JSON data", dataJson);
+
+                bufferedWriter.write( dataJson );
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                // Get the server response
+                InputStream inputStream = httpURLConnection.getInputStream();
+
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String line = "";
+
+                // Read Server Response
+                while((line = bufferedReader.readLine()) != null)
+                {
+                    // Append server response in string
+                    response+=line+"\n";
+                }
+
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Log.e("JSONParseHTTP ",response);
+
+            return response;
         }
     }
 
